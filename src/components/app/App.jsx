93b53d89bcac928a052cdc96850b9dde9c6674c7
Jerud-Moyer <jerudmoyer@gmail.com>
2020-10-22 08:51:22 -0700

    this wierd

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import AllArticles from '../../containers/articles/AllArticles';
-import NewsSearch from '../../containers/news-search/NewsSearch';
+
 
 export default function App() {
   return <AllArticles />;
